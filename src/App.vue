cf61dafb6ee4e72648b84b884c7b27ff89af94e5
theam-dev <phearakroun@gmail.com>
2022-11-26 12:44:02 +0700

    main autocomplete

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    
+    <div style="padding:50px;">
+      <h2>Vue Mail Autocomplete</h2>
+      <EmailDropdown :domains="domains" :defaultDomains="defaultDomains" id="emaildropdownload"/>
+</div>
+   
+  </div>
+</template>
+
+<script>
+import EmailDropdown from "vue-email-dropdown";
+export default {
+  name: 'Vue-Mail-Autocompleted',
+  data(){
+    return {
+      domains: [
+        "yourcompany.com",
+        "gmx.de",
+        "googlemail.com",
+        "hotmail.fr",
+        "hotmail.it",
+        "web.de",
+        "yahoo.co.in",
+        "yahoo.com",
+        "yahoo.in"
+      ],
+      defaultDomains: ["gmail.com", "hotmail.com", "msn.com", "outlook.com", "yahoo.com"]
+    }
+  },
+  components: {
+    EmailDropdown
+  }
+}
+</script>
+<style>
+  #emaildropdownload{
+    height: 28px;
+  }
+</style>
